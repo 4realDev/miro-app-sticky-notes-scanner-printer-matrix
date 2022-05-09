b98f0782bc2f77bf181d06b86b392aa5a7deed56
Vladimir Brazhnik <54774817+4realDev@users.noreply.github.com>
2022-05-09 13:50:14 +0200

    Use customIcon, title and open LeftSidebar instead of openLibrary to keep app persistant

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,25 @@
-import toolbarIcon from './assets/toolbaricon.svg?raw';
+import customicon from './assets/customicon.svg?raw';
 import libraryIcon from './assets/libraryicon.svg?raw';
 
-miro.onReady(() => {
-  miro.initialize({
-    extensionPoints: {
-      toolbar: {
-        title: 'Create Miro App',
-        toolbarSvgIcon: toolbarIcon,
-        librarySvgIcon: libraryIcon,
-        async onClick() {
-          // Remember that 'app.html' resolves relative to index.js file. So app.html have to be in the /dist/ folder.
-          await miro.board.ui.openLibrary('app.html', {
-            title: 'Create Miro App',
-          });
-        },
-      },
-    },
-  });
+window.miro.onReady(() => {
+	console.log('miro onReady');
+	miro.initialize({
+		extensionPoints: {
+			toolbar: {
+				title: 'User Interaction Test',
+				toolbarSvgIcon: libraryIcon,
+				librarySvgIcon: customicon, // icon visible in toolbar >>
+
+				async onClick() {
+					// Remember that 'app.html' resolves relative to index.js file. So app.html have to be in the /dist/ folder.
+					// HTML UI Shows after clicking the app icon
+					// app.html calls the app.tsx script, which runs the app functionality
+					// miro.board.ui.openLibrary disappears after clicking on canvas
+					// therefore related script stops running and related listeners don’t listen anymore
+					// to prevent this behaviour, use openLeftSidebar, which stays open after clicking on canvas
+					await miro.board.ui.openLeftSidebar('app.html');
+				},
+			},
+		},
+	});
 });
