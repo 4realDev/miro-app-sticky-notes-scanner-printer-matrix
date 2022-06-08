aeb404f60edf0469b5cce0569a5bccc3358f4fa0
Vladimir Brazhnik <54774817+4realDev@users.noreply.github.com>
2022-06-08 15:19:58 +0200

    Update MatrixApp to first setup matrix and widgets and then sort them inside matrix

--- src/components/MatrixApp.tsx
+++ src/components/MatrixApp.tsx
@@ -12,102 +12,11 @@ const MatrixApp = () => {
 	const [inputXAxis, setInputXAxis] = useState('Importance');
 	const [inputYAxis, setInputYAxis] = useState('Difficulty');
 
+	let minCoorPosX: number | undefined = undefined;
+	let minCoorPosY: number | undefined = undefined;
+
 	const isNumeric = (str: string) => {
 		return !isNaN(parseFloat(str)); // ...and ensure strings of whitespace fail
-	};
-
-	const createCoordinateSystem = async (
-		minPosX: number | undefined,
-		minPosY: number | undefined,
-		inputXAxis: string,
-		inputYAxis: string,
-		widgetWidth: number,
-		widgetHeight: number,
-		widgets: SDK.IWidget[]
-	) => {
-		const totalWidgetWidth = widgets.length * widgetWidth;
-		const totalWidgetHeight = widgets.length * widgetHeight;
-		// (0,0) point of the coordinate system -> bottom left edge
-		const coordinateSystemOrigin = minPosY!! + totalWidgetHeight - widgetHeight / 2;
-
-		/*** CREATE AXIS ***/
-		// create Y-Axis
-		await miro.board.widgets.create({
-			type: 'LINE',
-			captions: [{ text: 'TEST' }],
-			startPosition: {
-				x: minPosX!! - widgetWidth / 2,
-				y: minPosY!! - widgetHeight,
-			},
-			endPosition: {
-				x: minPosX!! - widgetWidth / 2,
-				y: coordinateSystemOrigin,
-			},
-			style: {
-				lineColor: '#000000',
-				lineStyle: 2,
-				lineThickness: 2,
-				lineEndStyle: 0,
-				lineStartStyle: 8,
-				lineType: 0,
-			},
-		});
-
-		// create X-Axis
-		await miro.board.widgets.create({
-			type: 'LINE',
-			startPosition: {
-				x: minPosX!! - widgetWidth / 2,
-				y: coordinateSystemOrigin,
-			},
-			endPosition: {
-				x: minPosX!! + totalWidgetWidth,
-				y: coordinateSystemOrigin,
-			},
-			style: {
-				lineColor: '#000000',
-				lineStyle: 2,
-				lineThickness: 2,
-				lineEndStyle: 8,
-				lineStartStyle: 0,
-				lineType: 0,
-			},
-		});
-
-		/*** CREATE LABELS ***/
-		// Create X-Axis Label
-		// https://developers.miro.com/reference/text
-		// +20px is because of the positioning of the label underneith the x-axis
-		await miro.board.widgets.create({
-			type: 'TEXT',
-			x: minPosX!! + totalWidgetWidth / 2 - widgetWidth / 2,
-			y: coordinateSystemOrigin + 20,
-			width: 300,
-			scale: 1.2857142857142858,
-			style: {
-				bold: 1,
-				textAlign: 'c',
-				textColor: '#1a1a1a',
-			},
-			text: inputXAxis,
-		});
-
-		// Create Y-Axis Label
-		// -180px is because of the position changing caused by the rotation of the label
-		await miro.board.widgets.create({
-			type: 'TEXT',
-			x: minPosX!! - 180,
-			y: minPosY!! + totalWidgetHeight / 2 - widgetHeight / 2,
-			width: 300,
-			rotation: -90,
-			text: inputYAxis,
-			scale: 1.2857142857142858,
-			style: {
-				bold: 1,
-				textAlign: 'c',
-				textColor: '#1a1a1a',
-			},
-		});
 	};
 
 	const calculateMinPosYAndMinPosX = (
@@ -136,12 +45,32 @@ const MatrixApp = () => {
 		});
 	};
 
-	// adjust x position of widgets to be perfectly horizontal aligned by respecting the manually created order on x axis
-	const alignWidgetsHorizontally = (sortedSelectedWidgetsAfterXValue: SDK.IWidget[], minPosX: number) => {
+	// adjust x any y position of widgets to be perfectly horizontal and vertical aligned in a line by respecting the manually created order on x axis
+	const alignWidgetsHorizontallyAndVerticallyInLine = (
+		sortedSelectedWidgetsAfterXValue: SDK.IWidget[],
+		minPosX: number,
+		minPosY: number
+	) => {
 		let widgetTagCount = -1;
 		sortedSelectedWidgetsAfterXValue.forEach(async (widget) => {
 			widgetTagCount = widgetTagCount + 1;
-			widget.bounds.x = minPosX + widget.bounds.width * widgetTagCount;
+			await miro.board.widgets.update({
+				...widget,
+				x: minPosX + widget.bounds.width * widgetTagCount,
+				y: minPosY,
+			});
+		});
+	};
+
+	// adjust x position of widgets to be perfectly horizontal aligned by respecting the manually created order on x axis
+	const alignWidgetsHorizontally = async (sortedSelectedWidgetsAfterXValue: SDK.IWidget[], minCoorPosX: number) => {
+		let widgetTagCount = -1;
+		sortedSelectedWidgetsAfterXValue.forEach(async (widget) => {
+			widgetTagCount = widgetTagCount + 1;
+			await miro.board.widgets.update({
+				...widget,
+				x: minCoorPosX + widget.bounds.width * widgetTagCount,
+			});
 		});
 	};
 
@@ -173,10 +102,9 @@ const MatrixApp = () => {
 
 	// vertical alignment according to the sorting
 	// update every widget in sorted array with new sorted y position and aligned x position
-	const alignWidgetsVertically = (
+	const alignWidgetsVerticallyAfterNumericTag = async (
 		sortedSelectedWidgetsAfterYValue: SDK.IWidget[],
-		minPosY: number,
-		widgetHeight: number
+		minCoorPosY: number
 	) => {
 		let widgetTagCount = -1;
 		sortedSelectedWidgetsAfterYValue.forEach(async (widget) => {
@@ -184,19 +112,118 @@ const MatrixApp = () => {
 			await miro.board.widgets.update({
 				...widget,
 				x: widget.bounds.x,
-				y: minPosY!! + widgetHeight * widgetTagCount,
+				y: minCoorPosY!! + widget.bounds.height * widgetTagCount + widget.bounds.height / 2,
 			});
 		});
 	};
 
-	// TODO: CORRECT BUG: Fails when multiple widgets have same number -> first number is taken into account, rest is ignored
-	// works only for one type of widget because of widget.bound.width calculation
-	// ignores other widgets which have the same number tag -> only sorts first one
-	// works with number tag and other tags as well
-	const getNumericTags = async () => {
-		const allSelectedWidgets = await miro.board.selection.get();
-		const allTags = await miro.board.tags.get();
+	const createCoordinateSystem = async (
+		minWidgetPosX: number | undefined,
+		minWidgetPosY: number | undefined,
+		inputXAxis: string,
+		inputYAxis: string,
+		widgetWidth: number,
+		widgetHeight: number,
+		widgets: SDK.IWidget[]
+	) => {
+		const totalWidgetWidth = widgets.length * widgetWidth;
+		const totalWidgetHeight = widgets.length * widgetHeight;
+		const paddingToWidgets = widgetHeight;
+		const distanceTextToAxis = 20;
 
+		// bottom left edge point of the coordinate system
+		// TODO: Understand why I have to subtract "widgetWidth / 2"
+		const coorOriginX = minWidgetPosX!! - widgetWidth / 2;
+		const coorOriginY = minWidgetPosY!! - totalWidgetHeight - paddingToWidgets;
+
+		/*** CREATE AXIS ***/
+		// create Y-Axis
+		await miro.board.widgets.create({
+			type: 'LINE',
+			startPosition: {
+				x: coorOriginX,
+				y: coorOriginY,
+			},
+			endPosition: {
+				x: coorOriginX,
+				y: minWidgetPosY!! - paddingToWidgets,
+			},
+			style: {
+				lineColor: '#000000',
+				lineStyle: 2,
+				lineThickness: 2,
+				lineEndStyle: 8,
+				lineStartStyle: 0,
+				lineType: 0,
+			},
+		});
+
+		// create X-Axis
+		await miro.board.widgets.create({
+			type: 'LINE',
+			startPosition: {
+				x: coorOriginX,
+				y: coorOriginY,
+			},
+			endPosition: {
+				x: coorOriginX + totalWidgetWidth,
+				y: coorOriginY,
+			},
+			style: {
+				lineColor: '#000000',
+				lineStyle: 2,
+				lineThickness: 2,
+				lineEndStyle: 8,
+				lineStartStyle: 0,
+				lineType: 0,
+			},
+		});
+
+		/*** CREATE LABELS ***/
+		// Create X-Axis Label
+		// https://developers.miro.com/reference/text
+		// -20px is because of the positioning of the label underneith the x-axis
+		await miro.board.widgets.create({
+			type: 'TEXT',
+			x: coorOriginX + totalWidgetWidth / 2,
+			y: coorOriginY - distanceTextToAxis,
+			width: 300,
+			scale: 1.2857142857142858,
+			style: {
+				bold: 1,
+				textAlign: 'c',
+				textColor: '#1a1a1a',
+			},
+			text: inputXAxis,
+		});
+
+		// Create Y-Axis Label
+		// -180px is because of the position changing caused by the rotation of the label
+		await miro.board.widgets.create({
+			type: 'TEXT',
+			x: coorOriginX - distanceTextToAxis,
+			y: coorOriginY + totalWidgetHeight / 2,
+			width: 300,
+			rotation: -90,
+			text: inputYAxis,
+			scale: 1.2857142857142858,
+			style: {
+				bold: 1,
+				textAlign: 'c',
+				textColor: '#1a1a1a',
+			},
+		});
+
+		miro.board.viewport.set(
+			{ x: coorOriginX, y: coorOriginY, width: totalWidgetWidth, height: totalWidgetHeight },
+			{ padding: { top: 50, bottom: 150, left: 0, right: 0 }, animationTimeInMS: 200 }
+		);
+
+		return [coorOriginX, coorOriginY];
+	};
+
+	const setupMatrixAndWidgets = async () => {
+		const allSelectedWidgets = await miro.board.selection.get();
 		let minPosX: number | undefined = undefined;
 		let minPosY: number | undefined = undefined;
 
@@ -206,29 +233,52 @@ const MatrixApp = () => {
 		const widgetWidth = allSelectedWidgets[0].bounds.width;
 
 		[minPosX, minPosY] = calculateMinPosYAndMinPosX(allSelectedWidgets, minPosX, minPosY);
-
 		// SORTING AND ALIGNING ON XAXIS
 		const sortedSelectedWidgetsAfterXValue = sortWidgetsOnXAxis(allSelectedWidgets);
-		alignWidgetsHorizontally(sortedSelectedWidgetsAfterXValue, minPosX);
+		alignWidgetsHorizontallyAndVerticallyInLine(sortedSelectedWidgetsAfterXValue, minPosX, minPosY);
 
-		// add tag number and widget in separated array to be able to sort this array in next step
-		const sortedSelectedWidgetsAfterYValueWithNumericTag = addNumericTags(allTags, allSelectedWidgets);
-
-		const sortedSelectedWidgetsAfterYValue = sortWidgetsOnYAxisAfterNumericTag(
-			sortedSelectedWidgetsAfterYValueWithNumericTag
-		);
-
-		alignWidgetsVertically(sortedSelectedWidgetsAfterYValue, minPosY, widgetHeight);
-
-		await createCoordinateSystem(
+		// returns minCoorPosX and minCoorPosY from coodinate system
+		// needed for sorting and alignment of widgets in second step
+		[minCoorPosX, minCoorPosY] = await createCoordinateSystem(
 			minPosX,
 			minPosY,
 			inputXAxis,
 			inputYAxis,
 			widgetWidth,
 			widgetHeight,
-			sortedSelectedWidgetsAfterYValue
+			allSelectedWidgets
 		);
+	};
+
+	// TODO: CORRECT BUG: Fails when multiple widgets have same number -> first number is taken into account, rest is ignored
+	// works only for one type of widget because of widget.bound.width calculation
+	// ignores other widgets which have the same number tag -> only sorts first one
+	// works with number tag and other tags as well
+	const sortMatrixAndWidgets = async () => {
+		const allSelectedWidgets = await miro.board.selection.get();
+		const allTags = await miro.board.tags.get();
+
+		if (minCoorPosX === undefined && minCoorPosY === undefined) {
+			alert('You must setup a Matrix first, before you can sort the Matrix');
+			return;
+		}
+
+		// SORTING AND ALIGNING ON XAXIS
+		const sortedSelectedWidgetsAfterXValue = sortWidgetsOnXAxis(allSelectedWidgets);
+		alignWidgetsHorizontally(sortedSelectedWidgetsAfterXValue, minCoorPosX!!);
+
+		// SORTING AND ALIGNING ON YAXIS
+		// add tag number and widget in separated array to be able to sort this array in next step
+		const sortedSelectedWidgetsAfterXAndYValueWithNumericTag = addNumericTags(
+			allTags,
+			sortedSelectedWidgetsAfterXValue
+		);
+
+		const sortedSelectedWidgetsAfterXAndYValue = sortWidgetsOnYAxisAfterNumericTag(
+			sortedSelectedWidgetsAfterXAndYValueWithNumericTag
+		);
+
+		alignWidgetsVerticallyAfterNumericTag(sortedSelectedWidgetsAfterXAndYValue, minCoorPosY!!);
 	};
 
 	return (
@@ -242,8 +292,11 @@ const MatrixApp = () => {
 				<label>y-Axis: </label>
 				<input style={inputStyle} value={inputYAxis} onChange={(e) => setInputYAxis(e.target.value)} />
 			</div>
-			<button style={buttonStyle} onClick={() => getNumericTags()}>
-				Sort in order
+			<button style={buttonStyle} onClick={() => setupMatrixAndWidgets()}>
+				Setup Matrix
+			</button>
+			<button style={buttonStyle} onClick={() => sortMatrixAndWidgets()}>
+				Sort Matrix
 			</button>
 		</div>
 	);
