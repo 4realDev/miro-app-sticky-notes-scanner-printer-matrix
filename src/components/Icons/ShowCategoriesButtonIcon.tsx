f4ca04ab73f974541e2f49bfdf4c95dcbc593f0b
Vladimir Brazhnik <vladimir.brazhnik@cando-image.com>
2023-02-03 21:23:00 +0100

    Add icons for wizard

---
+++ src/components/Icons/ShowCategoriesButtonIcon.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const showCategoriesButtonIcon = ({ ...props }) => {
+	return (
+		<svg width='21' height='20' viewBox='0 0 21 20' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
+			<path
+				d='M3.91135 4.16659C3.91135 3.70635 4.28444 3.33325 4.74468 3.33325H16.4113C16.8716 3.33325 17.2447 3.70635 17.2447 4.16659V5.83325C17.2447 6.29349 16.8716 6.66659 16.4113 6.66659H4.74468C4.28444 6.66659 3.91135 6.29349 3.91135 5.83325V4.16659Z'
+				stroke='white'
+				strokeWidth='2'
+				strokeLinecap='round'
+				strokeLinejoin='round'
+			/>
+			<path
+				d='M3.91135 10.8333C3.91135 10.373 4.28444 9.99992 4.74468 9.99992H9.74468C10.2049 9.99992 10.578 10.373 10.578 10.8333V15.8333C10.578 16.2935 10.2049 16.6666 9.74468 16.6666H4.74468C4.28444 16.6666 3.91135 16.2935 3.91135 15.8333V10.8333Z'
+				stroke='white'
+				strokeWidth='2'
+				strokeLinecap='round'
+				strokeLinejoin='round'
+			/>
+			<path
+				d='M13.9113 10.8333C13.9113 10.373 14.2844 9.99992 14.7447 9.99992H16.4113C16.8716 9.99992 17.2447 10.373 17.2447 10.8333V15.8333C17.2447 16.2935 16.8716 16.6666 16.4113 16.6666H14.7447C14.2844 16.6666 13.9113 16.2935 13.9113 15.8333V10.8333Z'
+				stroke='white'
+				strokeWidth='2'
+				strokeLinecap='round'
+				strokeLinejoin='round'
+			/>
+		</svg>
+	);
+};
+
+export default showCategoriesButtonIcon;
---
+++ src/components/Icons/SortByImportanceButtonIcon.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const SortByImportanceButtonIcon = ({ ...props }) => {
+	return (
+		<svg width='21' height='20' viewBox='0 0 21 20' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
+			<path
+				d='M7.19267 10.8333V9.99992M10.526 10.8333V8.33325M13.8593 10.8333V6.66659M7.19267 17.4999L10.526 14.1666L13.8593 17.4999M3.026 3.33325H18.026M3.85933 3.33325H17.1927V13.3333C17.1927 13.7935 16.8196 14.1666 16.3593 14.1666H4.69267C4.23243 14.1666 3.85933 13.7935 3.85933 13.3333V3.33325Z'
+				stroke='white'
+				strokeWidth='2'
+				strokeLinecap='round'
+				strokeLinejoin='round'
+			/>
+		</svg>
+	);
+};
+
+export default SortByImportanceButtonIcon;
