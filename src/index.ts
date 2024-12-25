2991dad53cdc818e7da825385e4142ccf84a784d
Vladimir Brazhnik <vladimir.brazhnik@cando-image.com>
2024-12-25 01:27:52 +0100

    Remove unnecessary code

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import customicon from './assets/customicon.svg?raw';
-import libraryIcon from './assets/libraryicon.svg?raw';
 import './fonts/Heebo/Heebo-Regular.ttf';
 import './fonts/Heebo/Heebo-Bold.ttf';
 
@@ -20,26 +18,3 @@ async function init() {
 }
 
 init();
-
-// window.miro.onReady(() => {
-// 	console.log('miro onReady');
-// 	miro.initialize({
-// 		extensionPoints: {
-// 			toolbar: {
-// 				title: 'User Interaction Test',
-// 				toolbarSvgIcon: libraryIcon,
-// 				librarySvgIcon: customicon, // icon visible in toolbar >>
-
-// 				async onClick() {
-// 					// Remember that 'app.html' resolves relative to index.js file. So app.html have to be in the /dist/ folder.
-// 					// HTML UI Shows after clicking the app icon
-// 					// app.html calls the app.tsx script, which runs the app functionality
-// 					// miro.board.ui.openLibrary disappears after clicking on canvas
-// 					// therefore related script stops running and related listeners don’t listen anymore
-// 					// to prevent this behaviour, use openLeftSidebar, which stays open after clicking on canvas
-// 					await miro.board.ui.openLeftSidebar('app.html');
-// 				},
-// 			},
-// 		},
-// 	});
-// });
